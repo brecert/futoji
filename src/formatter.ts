eddf4fc24196ba9c8eab1d53e839b63662b76a62
Brecert <11599528+Brecert@users.noreply.github.com>
2019-03-28 21:47:05 -0400

    formatters can now have different opening and closing symbols

--- src/formatter.ts
+++ src/formatter.ts
@@ -1,43 +1,76 @@
+import { RequireAtLeastOne, Merge } from 'type-fest';
+
 /**
  * A transformer transforms what is found between the symbols
  */
-interface ITransformer {
+interface Transformer {
+
   /**
    * note: currently unused 
    */
   name: string
 
   /**
-   * The symbol to match when opening and closing transformation
+   * The symbols to match to start matching a transformation
    */
-  symbol: string
+  open: string
+
+  /**
+   * The symbols to match to stop matching a transformation 
+   */
+  close: string
+
+  /**
+   * If the transformed text is to be transformed again
+   */
+  recursive: boolean
+
+  /**
+   * The function to transform the text into something else
+   */
+  transformer: (text: string) => string
+}
+
+type TransformerOptions = Merge<Transformer, {
+  /**
+   * @depricated
+   * please use open instead, symbol is depricated
+   */
+  symbol?: string
+
+  /**
+   * The symbols to match to start matching a transformation
+   */
+  open?: string
+
+  /**
+   * The symbols to match to stop matching a transformation 
+   */
+  close?: string
 
   /**
    * If the transformed text is to be transformed again
    */
   recursive?: boolean
-
-  /**
-   * The transform the text into something else
-   */
-  transformer: (text: string) => string
-}
+}>
 
 /**
  * The formatter class contains transformers to transform and format code
  */
 export default class Formatter {
-  transformers: ITransformer[] = []
+  transformers: Transformer[] = []
 
   /**
    * Define the symbol and the transformer
    * Add a transformer to transform code when formatting text
    * Transformers are are used in the order added so make sure to add transformer that may have conflicting syntax in the correct order
    */
-  addTransformer(params: ITransformer) {
-    this.transformers.push({...{
-      recursive: true
-    },...params})
+  addTransformer(params: TransformerOptions) {
+    this.transformers.push(Object.assign({
+      recursive: true,
+      open: <string>(params.symbol),
+      close: <string>(params.symbol || params.open)
+    }, params))
   }
 
   /**
@@ -52,8 +85,8 @@ export default class Formatter {
     // todo: change to slice or something
     function accept(string: string, offset = 0) {
       let pt = pos + offset
-      return string.split('').every((c, i) => {
-        return text[pt + i] === c
+      return string.split('').every((char, i) => {
+        return text[pt + i] === char
       })
     }
 
@@ -70,32 +103,34 @@ export default class Formatter {
     }
 
     while(pos < text.length) {
+
+      // return all transformers that start with the current char
       let matches = this.transformers.filter(transformer => {
-        return transformer.symbol.startsWith(text[pos])
+        return transformer.open.startsWith(text[pos])
       })
 
       if(matches.length > 0) {
         let matched = matches.some(match => {
-          let { name, symbol, transformer, recursive } = match
-          if(accept(symbol)) {
-            pos += symbol.length
+          let { name, open, close, transformer, recursive } = match
+          if(accept(open)) {
+            pos += open.length
 
             let fromPos = pos
             let toPos = pos
 
-            untilSymbol(symbol)
+            untilSymbol(close)
 
             // make sure the next symbol does not also match the current one
             // this stops `*italic**hi*` from working and makes sure there needs to be a different char inbetween
             // fixes bugs
-            while(accept(symbol, 1)) {
-              pos += symbol.length + 1
-              untilSymbol(symbol)
+            while(accept(close, 1)) {
+              pos += open.length + 1
+              untilSymbol(close)
             }
 
             toPos = pos
 
-            if(accept(symbol)) {
+            if(accept(close)) {
               let matchedText = text.slice(fromPos, toPos)
               let parsed = transformer(matchedText)
 
@@ -105,7 +140,7 @@ export default class Formatter {
 
               io.push(parsed)
 
-              pos += symbol.length
+              pos += close.length
               lastSlice = pos
               return true
             }
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -3,47 +3,117 @@ import Formatter from '../src/formatter'
 import { expect } from 'chai' 
 
 describe('Formatter', function() {
-  let formatter = new Formatter()
+  let markdown = new Formatter()
+  let openClose = new Formatter()
+
   it('should be created', function() {
-    expect(formatter).to.exist
+    expect(markdown).to.exist
+    expect(openClose).to.exist
   })
 
   describe('#addFormat()', function() {
-    it('should add the transformer to formatter', function() {
-      formatter.addTransformer({
+    it('should add the transformer to the formatter', function() {
+      markdown.addTransformer({
         name: 'italic',
         symbol: '*',
         transformer: text => `<i>${text}</i>`
       })
-      expect(formatter.transformers).to.have.lengthOf(1)
+      expect(markdown.transformers).to.have.lengthOf(1)
     })
 
-    it('should add a non recursive block transformer to formatter', function() {
-      formatter.addTransformer({
+    it('should add a non recursive block transformer to the formatter', function() {
+      markdown.addTransformer({
         name: 'block',
         symbol: '`',
         recursive: false,
         transformer: text => `<code>${text}</code>`
       })
-      expect(formatter.transformers.filter(t => t.name === 'block')[0].recursive).to.equal(false)
+      expect(markdown.transformers.filter(t => t.name === 'block')[0].recursive).to.equal(false)
+    })
+
+    it('should add a transformer that opens', function() {
+      openClose.addTransformer({
+        name: "tilda",
+        open: "~",
+        transformer: text => `[tilda:${text}]`
+      })
+    })
+
+    it('should add a transformer that opens and closes', function() {
+      openClose.addTransformer({
+        name: "emoji",
+        open: '<',
+        close: '>',
+        transformer: text => `[emoji:${text}]`
+      })
+    })
+
+    it('should add a transformer that closes with a space', function() {
+      openClose.addTransformer({
+        name: "header",
+        open: '#',
+        close: ' ',
+        transformer: text => `[header:${text}]`
+      })
+    })
+
+    it('should add a colliding transformer that closes with a space', function() {
+      openClose.addTransformer({
+        name: "green",
+        open: '> ',
+        close: ' ',
+        transformer: text => `[green:${text}]`
+      })
     })
   })
 
   describe('#format()', function() {
-    it('should format italic', function() {
-      expect(formatter.format('*italic text*')).to.equal('<i>italic text</i>')
+    it('should format', function() {
+      expect(markdown.format('*italic text*')).to.equal('<i>italic text</i>')
     })
 
-    it('should format italic and normal text', function() {
-      expect(formatter.format('normal text *italic text*')).to.equal('normal text <i>italic text</i>')
+    it('should format with normal text', function() {
+      expect(markdown.format('normal text *italic text*')).to.equal('normal text <i>italic text</i>')
     })
 
-    it('should format block', function() {
-      expect(formatter.format('`block text`')).to.equal('<code>block text</code>')
+    it('should format with different transformers', function() {
+      expect(markdown.format('`block text`')).to.equal('<code>block text</code>')
     })
 
-    it('should not format italic in block', function() {
-      expect(formatter.format('`block text *not italic text*`')).to.equal('<code>block text *not italic text*</code>')
+    it('should not format recursively if recursive is not enabled', function() {
+      expect(markdown.format('`block text *not italic text*`')).to.equal('<code>block text *not italic text*</code>')
+    })
+
+    it('should format using open', function() {
+      expect(openClose.format('~stuff~')).to.equal('[tilda:stuff]')
+    })
+
+    it('should format using open and close', function() {
+      expect(openClose.format('<smile>')).to.equal('[emoji:smile]')
+    })
+
+    it('should format using open and close recursively', function() {
+      expect(openClose.format('<smile~stuff~>')).to.equal('[emoji:smile[tilda:stuff]]')
+      expect(openClose.format('~stuff<smile>~')).to.equal('[tilda:stuff[emoji:smile]]')
+      expect(openClose.format('<~smile stuff~>')).to.equal('[emoji:[tilda:smile stuff]]')
+    })
+
+    it('should format using close with a space', function() {
+      expect(openClose.format('#title')).to.equal('#title')
+      expect(openClose.format('#title ')).to.equal('[header:title]')
+    })
+
+    it('should format with a colliding transformer that closes with a space', function() {
+      expect(openClose.format('> message ')).to.equal('[green:message]')
+      expect(openClose.format('<smile> > message ')).to.equal('[emoji:smile] [green:message]')    
+    })
+
+    it('should not format with a colliding transformer that closes with a space', function() {
+      expect(openClose.format('>message')).to.equal('>message')
+      expect(openClose.format('> message')).to.equal('> message')
+      expect(openClose.format('>message ')).to.equal('>message ')
+
+      expect(openClose.format('<smile>> message ')).to.equal('<smile>> message ')    
     })
   })
 })
